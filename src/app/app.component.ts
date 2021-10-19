0fd2d331d34049ce7351638e7352190e942a0cc4
DmitriyAionys <89926743+DmitriyAionys@users.noreply.github.com>
2021-10-19 10:33:44 +0300

    firebase injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'forms';
+
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDh6budoUwlSxQh7_peBnQvVrU_Xi06cdQ",
+      authDomain: "qatesttask-3bd9a.firebaseapp.com",
+      databaseURL: "https://qatesttask-3bd9a-default-rtdb.firebaseio.com",
+      projectId: "qatesttask-3bd9a",
+      storageBucket: "qatesttask-3bd9a.appspot.com",
+      messagingSenderId: "88188514881",
+      appId: "1:88188514881:web:e29fc3e5e0c521170c4cc0",
+      measurementId: "G-HKV70M7SVT"
+    };
+    
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app)
+  }
 }
